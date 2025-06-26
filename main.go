e915e271979fd887436273a6e203198ab9358427
xrt19 <irfanhera17@gmail.com>
2025-06-26 17:37:28 +0700

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	// Menampilkan pesan bahwa server sedang berjalan.
 	fmt.Println("Server akan berjalan di port 8080...")
-	fmt.Println("tes pembeda kamis 26 juni 2025 jam 17.19")
+	fmt.Println("tes pembeda kamis 26 juni 2025 jam 17.38")
 
 	// Menangani permintaan ke path root "/"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
